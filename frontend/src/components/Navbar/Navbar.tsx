96eca649224042c34b96a9e8032fae3645da2c4f
cdlavacudeg <cristianlav10@gmail.com>
2022-06-25 12:42:58 -0500

    [Add] Img Assets

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
-// import './Navbar.scss'
-export const Navbar = (props : {}) => {
+import {images} from '../../constants'
+import './Navbar.scss'
+
+export const Navbar :React.FC = () => {
+
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <div className="nav-logo" id="navlogo">
+        &lt; cdlavacudeg / &gt;
+      </div>
+      <div className="nav-menu" id="navMenu">
+                <button className="nav-toggle" >
+                    <i className="fa fa-bars"></i>
+                </button>
+                <ul className="nav--list">
+                    <li> <a href="#home">Home</a></li>
+                    <li> <a href="#services">My Services</a></li>
+                    <li> <a href="#about">About me</a></li>
+                    <li> <a href="#work">My Work</a></li>
+                </ul>
+            </div>
+    </nav>
   )
 }
